d74e214d65dbce9d2b097f7b1631e2106a413df7
sanketbj <jamkhedkarsanket1999@gmail.com>
2024-12-22 16:53:00 +0530

    Create automation6.sh

---
+++ automation6.sh
@@ -0,0 +1,13 @@
+/usr/sbin/sendmail -i -t <<
+Subject: $1 server process status
+From: singampallipraveenkumar@gmail.com
+To: singampallipraveenkumar@gmail.com
+From: 
+To:
+Hi Team,
+Please check for $1 service in TEST server which has $3 
+process running with below list of KIT IDs
+$2
+Regards,
+ShineInCareer 
+MESSAGE_END
